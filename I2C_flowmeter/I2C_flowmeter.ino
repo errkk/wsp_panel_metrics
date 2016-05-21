1e5140238804536a698457865703d366625ad412
Eric George <eric@pokelondon.com>
2016-05-21 18:11:44 +0100

    moving I2c flowmeter into this repo

---
+++ I2C_flowmeter/I2C_flowmeter.ino
@@ -0,0 +1,99 @@
+#include <TinyWireS.h>
+
+// MAX INT 32767
+
+
+#ifndef TWI_RX_BUFFER_SIZE
+#define TWI_RX_BUFFER_SIZE (16)
+#endif
+
+#define ADDR 0x11
+#define PIN 4
+// (pin number 3, PB4)
+#define LED 3
+// (pin number 2, PB3)
+
+// Flow meter Variables
+uint8_t flowState = 0;
+uint8_t lastFlowState = 0;
+
+unsigned long lastTick;
+unsigned long timeBetweenTicks = 0;
+const int litersPerTick = 10;
+
+volatile float litersPerSec = 0; // 4 bytes
+
+// Buffers for sending int over I2C
+boolean firstbyte = true;
+byte lowByte;
+byte highByte;
+
+/**
+ * This is called for each read request we receive, never put more than one byte of data (with TinyWireS.send) to the 
+ * send-buffer when using this callback
+ */
+void requestEvent()
+{  
+
+  union float2bytes { float f; char b[sizeof(float)]; };
+  
+  float2bytes f2b;
+  
+  f2b.f = litersPerSec;
+
+  if(firstbyte == true){     // on the first byte we do the math
+    // x 100
+    int intlitersPerSecX10 = (int) litersPerSec * 10;
+    lowByte = (byte) (intlitersPerSecX10 & 0xff);
+    highByte = (byte) ((intlitersPerSecX10 >> 8) & 0xff);
+    firstbyte = false;      //so next time though we send the next byte    
+    TinyWireS.send(lowByte);
+  } 
+  else {
+    TinyWireS.send(highByte);
+    firstbyte = true;
+  }
+}
+
+void setup() {
+  TinyWireS.begin(ADDR);
+  TinyWireS.onRequest(requestEvent);
+
+  pinMode(PIN, INPUT);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+}
+
+void loop() {
+  flowState = digitalRead(PIN);
+  
+  if (flowState != lastFlowState) {
+    digitalWrite(LED, LOW);
+    if (flowState == LOW) {
+      
+      unsigned long newTime = millis();
+      
+      timeBetweenTicks = newTime - lastTick;
+
+      if(timeBetweenTicks > 100) {
+        lastTick = newTime;
+        litersPerSec = litersPerTick / (timeBetweenTicks / 1000.0);
+        digitalWrite(LED, HIGH);
+      }
+    }
+  }
+  
+  
+  // save the current state as the last state,
+  // for next time through the loop
+  lastFlowState = flowState;
+
+  /**
+   * This is the only way we can detect stop condition (http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&p=984716&sid=82e9dc7299a8243b86cf7969dd41b5b5#984716)
+   * it needs to be called in a very tight loop in order not to miss any (REMINDER: Do *not* use delay() anywhere, use tws_delay() instead).
+   * It will call the function registered via TinyWireS.onReceive(); if there is data in the buffer on stop.
+   */
+  TinyWireS_stop_check();
+}
